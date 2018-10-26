7966bf482370f969e593677f41c5b1618c198f78
tianhe1986 <w1s2j3229@163.com>
2018-10-26 17:06:03 +0800

    UInt32 handle.
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- src/FatBinaryBuffer.php
+++ src/FatBinaryBuffer.php
@@ -4,5 +4,97 @@ namespace FatBinaryBuffer;
 
 class FatBinaryBuffer
 {
+    // real buffer
+    protected $buffer = '';
     
+    // now position
+    protected $offset = 0;
+    
+    // buffer length
+    protected $len = 0;
+    
+    // big endian or little endian
+    protected $isBigEndian = true;
+    
+    protected $isSystemBigEndian;
+    
+    public function __construct($isBigEndian = true)
+    {
+        $this->isBigEndian = $isBigEndian;
+        $this->isSystemBigEndian = (pack("L", 1) === pack("N", 1));
+    }
+    
+    public function setBuffer($buffer)
+    {
+        $this->buffer = $buffer;
+        $this->len = strlen($buffer);
+        $this->offset = 0;
+    }
+    
+    public function rewind()
+    {
+        $this->setOffset(0);
+    }
+    
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+    
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+    
+    public function getLength()
+    {
+        return $this->len;
+    }
+    
+    public function readUInt32()
+    {
+        $str = $this->readFromBuffer(4);
+        $array = unpack($this->isBigEndian ? "Nval" : "Vval", $str);
+        return $array["val"];
+    }
+    
+    public function writeUInt32($val)
+    {
+        $str = pack($this->isBigEndian ? "N" : "V", $val);
+        $this->writeToBuffer($str);
+        
+        return $this;
+    }
+    
+    protected function readFromBuffer($len)
+    {
+        $toOffset = $this->offset + $len;
+        if ($toOffset > $this->len) {
+            throw new Exception("len exceed");
+        }
+        $str = substr($this->buffer, $this->offset, $len);
+        $this->offset = $toOffset;
+        
+        return $str;
+    }
+    
+    protected function writeToBuffer($val)
+    {
+        if ($this->offset === $this->len) { //在末尾
+            $this->buffer .= $val;
+            $this->offset = $this->len = ($this->offset + strlen($val));
+        } else { //拼接
+            $len = strlen($val);
+            $this->buffer = substr($this->buffer, 0, $this->offset) . $val . substr($this->buffer, $this->offset + $len);
+            $this->offset += $len;
+            if ($this->offset > $this->len) {
+                $this->len = $this->offset;
+            }
+        }
+    }
+    
+    public function getBuffer()
+    {
+        return $this->buffer;
+    }
 }
--- tests/FatBinaryBufferTest.php
+++ tests/FatBinaryBufferTest.php
@@ -7,5 +7,29 @@ use FatBinaryBuffer\FatBinaryBuffer;
 
 class FatExcelTest extends TestCase
 {
+    public function testUInt32()
+    {
+        $a = 123;
+        $binaryBufferBE = new FatBinaryBuffer(true);
+        $binaryBufferLE = new FatBinaryBuffer(false);
+        
+        $binaryBufferBE->writeUInt32($a);
+        $this->assertEquals(4, $binaryBufferBE->getLength());
+        $this->assertEquals(pack("N", $a), $binaryBufferBE->getBuffer());
+        
+        $binaryBufferLE->writeUInt32($a);
+        $this->assertEquals(4, $binaryBufferLE->getLength());
+        $this->assertEquals(pack("V", $a), $binaryBufferLE->getBuffer());
+        
+        $binaryBufferBE->rewind();
+        $binaryBufferLE->rewind();
+        
+        $this->assertEquals($a, $binaryBufferBE->readUInt32());
+        $this->assertEquals($a, $binaryBufferLE->readUInt32());
+    }
     
+    public function testMixedBigEndian()
+    {
+        
+    }
 }
