7ef6c97946b9d097c7ef7761235024a98ffe5898
tianhe1986 <w1s2j3229@163.com>
2018-10-26 13:57:16 +0800

    initial commit.
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

---
+++ src/FatBinaryBuffer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FatBinaryBuffer;
+
+class FatBinaryBuffer
+{
+    
+}
---
+++ tests/FatBinaryBufferTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FatBinaryBuffer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use FatBinaryBuffer\FatBinaryBuffer;
+
+class FatExcelTest extends TestCase
+{
+    
+}
